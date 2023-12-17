52d82ead5bff0a55dc1e58045b35c777638a3629
Kassio-Gandara-De-Souza <kassiocasual94@gmail.com>
2023-12-17 00:15:20 -0300

    modificação em schema.py

--- WebAuto-FastAPI-main/projeto_final_talentX/src/schemas/schema.py
+++ WebAuto-FastAPI-main/projeto_final_talentX/src/schemas/schema.py
@@ -1,11 +1,42 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 class User(BaseModel):
     id: Optional[str] = None
     name: str
     CPF: str
     tel: str
+
+class Usuario(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    cpf: str
+    email: str
+    telefone: str
+    senha: str
+    #produtos: List[ProdutoSimples] = []
+
+    class Config:
+        orm_mode = True
+
+
+class UsuarioSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class LoginData(BaseModel):
+    senha: str
+    email: str
+
+
+class LoginSucesso(BaseModel):
+    usuario: UsuarioSimples
+    access_token: str
     
 class Carro(BaseModel):
     id: Optional[str] = None
