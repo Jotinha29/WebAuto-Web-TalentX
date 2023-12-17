9d726190fd22f5b17ab557aab98801bdb9860a24
Kassio-Gandara-De-Souza <kassiocasual94@gmail.com>
2023-12-16 22:38:00 -0300

    pasta routers

--- WebAuto-FastAPI-main/projeto_final_talentX/routers/rotas_pedidos.py
+++ WebAuto-FastAPI-main/projeto_final_talentX/routers/rotas_pedidos.py
@@ -1,10 +1,15 @@
+
+
 from fastapi import APIRouter, status, Depends, HTTPException
 from typing import List
 from src.infra.sqlalchemy.config.database import get_db
+from src.schemas.schema import Compra
+
+
 
 router = APIRouter()
 
-@router.post('/pedidos')
+@router.post('/pedidos', status_code= status.HTTP_201_CREATED, response_model= None)
 def fazer_pedido():
     pass
 
